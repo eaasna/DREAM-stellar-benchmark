cc058e1354f28ebbdb339f5e13614412b17567f3
Evelin <evelin.aasna@fu-berlin.de>
2025-05-12 15:31:27 +0200

    Compare FPR by information content

--- work-python/kmer-occurrence-probability/get_tables.sh
+++ work-python/kmer-occurrence-probability/get_tables.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-set -e
+set -ex
+
+
+for er in 1 2 3; do
+	for inf in "0.35" "1.0"; do
+
+		dir="mouse/inf_cont$inf"
+		infile="${dir}/b4096_fpr0.005_l50_e$er.bin.split.err"
+		
+		grep -A 18 "\-FNR\-" ${infile} | grep -v "\-FNR\-" > ${dir}/fnr_e${er}.tsv
+		grep -A 18 "FP per pattern" ${infile} | grep -v "FP per pattern" > ${dir}/fpr_e${er}.tsv
+
+	done
+done
+
+exit 1
 
 size=100Mb
 infile=${size}.out
